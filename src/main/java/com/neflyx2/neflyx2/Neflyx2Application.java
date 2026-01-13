def6e9210e16e8c2b6d1fa44019ae9a469308e8d
Riu <riutordjaume@gmail.com>
2026-01-13 23:32:07 +0100

    primer commit

---
+++ src/main/java/com/neflyx2/neflyx2/Neflyx2Application.java
@@ -0,0 +1,13 @@
+package com.neflyx2.neflyx2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neflyx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neflyx2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/neflyx2/neflyx2/Neflyx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neflyx2.neflyx2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neflyx2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
